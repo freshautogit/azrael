fbd2d20124b59cde14b8c52f1e378c977a1a8ac8
Danila <d.borunov@freshauto.ru>
2020-03-13 13:55:24 +0000

    Configuration file for connecting to our services

--- azrael/back/config.py
+++ azrael/back/config.py
@@ -1,6 +1,13 @@
+import keyring
+
 ########################################################################################################################
-sql_host = '10.10.1.3'
-sql_user = 'postgres'
-sql_password = 'asd345TGB'
-sql_database = 'yellowpages'
+charset = 'utf8mb4'
+########################################################################################################################
+tracker_token = keyring.get_password('yandex_tracker', 'token')
+tracker_org_id = keyring.get_password('yandex_tracker', 'org_id')
+########################################################################################################################
+sql_host = keyring.get_password('postgresql', 'host')
+sql_user = keyring.get_password('postgresql', 'user')
+sql_password = keyring.get_password('postgresql', sql_user)
+sql_database = keyring.get_password('postgresql', 'db_name')
 ########################################################################################################################
