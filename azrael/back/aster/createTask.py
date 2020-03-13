eb946e68c50667bb4f6e878aef408b493e97e973
Danila <d.borunov@freshauto.ru>
2020-03-13 13:51:51 +0000

    Script is creating task to Yandex.Tracker by incall

---
+++ azrael/back/aster/createTask.py
@@ -0,0 +1,78 @@
+from sys import argv
+
+import psycopg2
+from yandex_tracker_client import TrackerClient
+import pymysql.cursors
+from back import config
+
+dbname = config.sql_database
+user = config.sql_user
+password = config.sql_password
+host = config.sql_host
+
+
+def get_user(number):
+    mail = ''
+    surname = ''
+    try:
+        connection = psycopg2.connect(dbname=dbname, user=user,
+                                      password=password, host=host)
+        with connection.cursor() as cursor:
+            sql = "select surname, name, email from phonebook where workphone = '{0}'".format(number)
+            cursor.execute(sql)
+
+            for row in cursor:
+                mail = row['email']
+                surname = row['surname'] + ' ' + row['name']
+
+        connection.close()
+        return mail, surname
+    except:
+        return mail, surname
+
+
+def get_assignee(number):
+    login = ''
+    try:
+        connection = psycopg2.connect(dbname=dbname, user=user,
+                                      password=password, host=host)
+        with connection.cursor() as cursor:
+            sql = "select email from phonebook where workphone = '{0}'".format(number)
+            cursor.execute(sql)
+
+            for row in cursor:
+                login = row['email']
+
+        connection.close()
+        return login[:login.index('@')]
+    except:
+        return login
+
+
+client = TrackerClient(token=config.tracker_token, org_id=config.tracker_org_id)
+
+
+def create(number_user, number_assignee):
+    email, fio = get_user(number_user)
+
+    if email == '':
+        fio = number_user
+
+    try:
+        print('https://tracker.yandex.ru/' + client.issues.create(
+            queue='helpdesk',
+            emailFrom=email,
+            summary='Звонок от {0}'.format(fio),
+            type={'name': 'Ticket'},
+            description='*Опиши проблему*',
+            assignee=get_assignee(number_assignee)
+        ).key)
+    except:
+        issue = client.issues.create(
+            queue='helpdesk',
+            emailFrom=email,
+            summary='Звонок от {0}'.format(fio),
+            type={'name': 'Ticket'},
+            description='*Опиши проблему*')
+        comment = issue.comments.create(text='#FIX\nЧто то пошло не так', summonees='v.gussarov')
+        print('https://tracker.yandex.ru/' + issue.key)
