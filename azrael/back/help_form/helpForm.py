318f022026acb49853774ab0726a7f56cc096001
Danila <d.borunov@freshauto.ru>
2020-03-17 12:55:28 +0000

    File has been replaced

--- azrael/back/help_form/helpForm.py
+++ azrael/back/help_form/helpForm.py
@@ -5,19 +5,19 @@ import Sql
 client = TrackerClient(token="AgAEA7qjC7i7AAW3zSixTG743kAdoEGtjtUF8hc", org_id="2120191")
 
 
-def acceptTask(request, ip):
+def accept_task(request, ip):
     priority = 'normal'
-    typeTask = 'Ticket'
+    type_task = 'Ticket'
 
     text = request.POST['ticketText'].capitalize() + '\n\n'
 
     if request.POST['subject'] == '1CD':
         queue = 'AA'
-        typeTask = 'Task'
+        type_task = 'Task'
         assignee = 'robotfresh'
     elif request.POST['subject'] == '1CP':
         queue = 'AC'
-        typeTask = 'Task'
+        type_task = 'Task'
         assignee = 'robotfresh'
     else:
         queue = 'HELPDESK'
@@ -45,56 +45,49 @@ def acceptTask(request, ip):
     if request.POST['localIp'] != '':
         text += 'Внутренний ip: ' + request.POST['localIp'] + '\n'
 
-    return createTask(queue, text, request.POST['email'], priority, typeTask, assignee)
+    return create_task(queue, text, request.POST['email'], priority, type_task, assignee)
 
 
-def createTask(queue, text, emailFrom, priority, typeTask, assignee):
-    pathToFiles = '/home/hack/help/media/files/'
+def create_task(queue, text, email_from, priority, type_task, assignee):
+    path_to_files = '/home/hack/help/media/files/'
 
-    print(os.listdir(pathToFiles))
+    print(os.listdir(path_to_files))
 
     files = []
 
-    for file in os.listdir(pathToFiles):
-        files.append(pathToFiles + file)
+    for file in os.listdir(path_to_files):
+        files.append(path_to_files + file)
 
-    pathToFile = checkFile(files)
+    path_to_file = check_file(files)
 
     key = (client.issues.create(
         queue=queue,
         summary=text[:50],
-        type={'name': typeTask},
-        emailFrom=emailFrom + '@freshauto.ru',
+        type={'name': type_task},
+        emailFrom=email_from + '@freshauto.ru',
         emailTo='vadimhacker.ru',
         assignee=assignee,
         description=text,
         priority=priority,
-        attachments=pathToFile
+        attachments=path_to_file
     ).key)
 
-    for file in pathToFile:
+    for file in path_to_file:
         os.remove(file)
 
     return key
 
 
-def getAssignee(ip):
-    if '185.216.81.72' in ip:
-        return '1130000033904338'
-    elif '185.216.81.77' in ip:
-        return '1130000033904458'
-
-
-def checkFile(pathToFiles):
+def check_file(path_to_files):
     apply = [".jpeg", ".jpg", ".gif", ".png", ".svg", ".avi", ".mp4", ".zip", ".rar", ".pdf", ".doc", ".docx", ".xls",
              ".xlsx", ".pptx", ".ppt", ".mp3", ".wav", ".odp", ".7z"]
-    finishPathToFile = []
+    finish_path_to_file = []
 
-    for test in pathToFiles:
+    for test in path_to_files:
 
         if test[test.rindex('.'):] in apply:
-            finishPathToFile.append(test)
+            finish_path_to_file.append(test)
         else:
             print("Error: {0}".format(test))
 
-    return finishPathToFile
+    return finish_path_to_file
