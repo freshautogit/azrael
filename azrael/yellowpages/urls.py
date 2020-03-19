81f5a570aa55239c691ea765a0a9dd2213b6f96d
Danila <d.borunov@freshauto.ru>
2020-03-19 17:47:56 +0300

    Changed linknames

--- azrael/yellowpages/urls.py
+++ azrael/yellowpages/urls.py
@@ -8,14 +8,14 @@ urlpatterns = [
     # в справочник со страницы авторизированного поиска
     path('search/', views.search, name='search'),
     path('addinfo/', views.add_info, name='add_info'),
-    path('search/add_user/', views.add_user, name='add_user'),
-    path('search/edit_user/', views.edit_user, name='edit_user'),
-    path('search/save_user/', views.save_user, name='save_user'),
-    path('search/del_user/', views.del_user, name='del_user'),
-    path('search/add_user/dropdown_request/', views.dropdown_request, name='dropdown_request'),
+    path('search/adduser/', views.add_user, name='add_user'),
+    path('search/edituser/', views.edit_user, name='edit_user'),
+    path('search/saveuser/', views.save_user, name='save_user'),
+    path('search/deluser/', views.del_user, name='del_user'),
+    path('search/adduser/dropdown_request/', views.dropdown_request, name='dropdown_request'),
 
     # Добавить новую запись в справочник с главной страницы
-    path('add_user/', views.add_user, name='add_user'),
+    path('adduser/', views.add_user, name='add_user'),
 
     # Обычный поиск
     path('s/', views.regular_search, name='regular_search'),
@@ -27,10 +27,10 @@ urlpatterns = [
     # Добавить новую запись в справочник со страницы поиска
     # Удалить, изменить и сохранить запись в справочнике
     # со страницы обычного поиска
-    path('s/add_user/', views.add_user, name='add_user'),
-    path('s/del_user/', views.del_user, name='del_user'),
-    path('s/edit_user/', views.edit_user, name='edit_user'),
-    path('s/save_user/', views.save_user, name='save_user'),
+    path('s/adduser/', views.add_user, name='add_user'),
+    path('s/deluser/', views.del_user, name='del_user'),
+    path('s/edituser/', views.edit_user, name='edit_user'),
+    path('s/saveuser/', views.save_user, name='save_user'),
     #############################################################
 
     path('dropdown_request/', views.dropdown_request, name='dropdown_request'),
