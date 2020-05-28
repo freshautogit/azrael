410aade884ba3559a7c21730a7344b64334c7001
Gussarov <vadimgussarov@gmail.com>
2020-05-28 11:06:30 +0300

    add wbhook for bots

--- bots/views.py
+++ bots/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from back.aster import createTask
 from back.bi import json_in_bi
 from back.zabbix import zabbix
+from back.webhook import wbhook
 import json
 
 
@@ -28,3 +29,16 @@ def zabbix_create_task(request):
     if json_task['name'] == 'zabbix':
         zabbix.create_task(json_task['subject'], json_task['message'], json_task['login'])
     return HttpResponse(status=200)
+
+
+def new_task_no_assignee(request):
+    json_task = json.loads(request.body.decode())
+    if json_task['name'] == 'tracker':
+        wbhook.new_task_no_assignee(json_task['key'])
+    return HttpResponse(status=200)
+
+
+def new_assignee(request):
+    json_task = json.load(request.body.decode())
+    if json_task['name'] == 'tracker':
+        wbhook.new_assignee(json_task['key'])
