89a4425c4eef402ba4162ca612d5650a66bd4792
Gussarov <vadimgussarov@gmail.com>
2020-06-03 00:22:52 +0300

    Created method to close the task from zabbix

--- back/zabbix/zabbix.py
+++ back/zabbix/zabbix.py
@@ -5,18 +5,43 @@ from back import config
 client = TrackerClient(token=config.tracker_token, org_id=config.tracker_org_id)
 
 
-def create_task(service, type_error, ip, office):
+def create_task(subject, message, login):
     try:
         client.issues.create(
             queue='TI',
-            summary='{0} - {1}'.format(service, type_error),
-            description='IP - {0}'.format(ip),
-            tags=[office]
+            summary='{0}'.format(subject),
+            description='{0}'.format(message),
+            assignee='{0}'.format(login),
+            components=['zabbix']
         )
     except Exception as e:
-        client.issues.create(
-            queue='DEV',
-            summary='Error from zabbix',
-            assignee='v.gussarov',
-            description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
-        )
+        if e.status_code == 429:
+            create_task(subject, message, login)
+        else:
+            client.issues.create(
+                queue='DEV',
+                summary='Error from zabbix',
+                assignee='v.gussarov',
+                description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
+            )
+
+
+def closed_task(id):
+    try:
+        id = id[:id.find('Resolved')].strip()
+        find = 'Summary: {0} Author: "robotfresh" Status: backlog'.format(id)
+        issues = client.issues.find(find)
+        for issue in issues:
+            transition = issue.transitions['resolved']
+            issue.comments.create(text='Fixed', summonees=['v.gussarov'])
+            transition.execute()
+    except Exception as e:
+        if e.status_code == 429:
+            closed_task(id)
+        else:
+            client.issues.create(
+                queue='DEV',
+                summary='Error from zabbix',
+                assignee='v.gussarov',
+                description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
+            )
