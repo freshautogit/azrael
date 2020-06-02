bd1886982491ce196f466a02feca29701ee7661d
Gussarov <vadimgussarov@gmail.com>
2020-06-02 23:29:27 +0300

    Created method to close the task from zabbix

--- back/zabbix/zabbix.py
+++ back/zabbix/zabbix.py
@@ -11,28 +11,37 @@ def create_task(subject, message, login):
             queue='TI',
             summary='{0}'.format(subject),
             description='{0}'.format(message),
-            assignee='{0}'.format(login)
+            assignee='{0}'.format(login),
+            components=['zabbix']
         )
     except Exception as e:
-        client.issues.create(
-            queue='DEV',
-            summary='Error from zabbix',
-            assignee='v.gussarov',
-            description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
-        )
+        if e.status_code == 429:
+            create_task(subject, message, login)
+        else:
+            client.issues.create(
+                queue='DEV',
+                summary='Error from zabbix',
+                assignee='v.gussarov',
+                description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
+            )
 
 
 def closed_task(id):
     try:
-        find = 'Summary: {0} Author: "robotfresh" Resolution: empty()'.format(id)
+        id = id[:id.find('Resolved')].strip()
+        find = 'Summary: {0} Author: "robotfresh" Status: backlog'.format(id)
         issues = client.issues.find(find)
         for issue in issues:
-            transition = issue.transitions['close']
-            transition.execute(comment='Fixed', resolution='fixed')
+            transition = issue.transitions['resolved']
+            issue.comments.create(text='Fixed', summonees=['v.gussarov'])
+            transition.execute()
     except Exception as e:
-        client.issues.create(
-            queue='DEV',
-            summary='Error from zabbix',
-            assignee='v.gussarov',
-            description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
-        )
+        if e.status_code == 429:
+            closed_task(id)
+        else:
+            client.issues.create(
+                queue='DEV',
+                summary='Error from zabbix',
+                assignee='v.gussarov',
+                description='#FIX\n\nЧто то пошло не так\n{0}'.format(e)
+            )
